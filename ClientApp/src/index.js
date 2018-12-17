2324cfcba48461062dea02b5962fef7e1b1b69c9
Victoria Oh <ohvictori@gmail.com>
2018-12-17 14:47:12 -0800

    Remove unused template files. Create Header component and verify it renders. Create Assets directory.

--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -1,19 +1,20 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
-import './index.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import "bootstrap/dist/css/bootstrap.css";
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./components/App";
+import registerServiceWorker from "./registerServiceWorker";
 
-const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
-const rootElement = document.getElementById('root');
+const baseUrl = document.getElementsByTagName("base")[0].getAttribute("href");
+const rootElement = document.getElementById("root");
 
 ReactDOM.render(
   <BrowserRouter basename={baseUrl}>
     <App />
   </BrowserRouter>,
-  rootElement);
+  rootElement
+);
 
+//The service worker is a web API that helps you cache your assets and other files so that when the user is offline or on slow network, he/she can still see results on the screen, as such, it helps you build a better user experience
 registerServiceWorker();
