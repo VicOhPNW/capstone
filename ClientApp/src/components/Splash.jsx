a22225d94d8071cb08d35ebd3cd136927182c9da
Victoria Oh <ohvictori@gmail.com>
2018-12-18 13:24:06 -0800

    Add carousel functionality to Splash component.

--- ClientApp/src/components/Splash.jsx
+++ ClientApp/src/components/Splash.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import "./Splash.css";
+import carousel2 from "../assets/img/espresso.jpg";
+import { Carousel } from "react-bootstrap";
 
 function Splash() {
   return (
     <div className="splashPage">
       <p>Splash stuff</p>
+
+      <Carousel className="splashCarousel">
+        {/* <Carousel.Item>
+          <img src={carousel1} alt="mt rainier" />
+        </Carousel.Item> */}
+        <Carousel.Item>
+          <img src={carousel2} alt="mt rainier" />
+        </Carousel.Item>
+      </Carousel>
     </div>
   );
 }
