35e36d41978c3bf98ee504f9eb48a828859333c5
Victoria Oh <ohvictori@gmail.com>
2018-12-17 16:55:37 -0800

    Create Contact Component and verify displays.

--- ClientApp/src/components/App.jsx
+++ ClientApp/src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./Header";
 import "./App.css";
+import Contact from "./Contact";
 
 function App() {
   return (
     <div>
       <Header />
+      <Contact />
     </div>
   );
 }
---
+++ ClientApp/src/components/Contact.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div>
+      stuff
+    </div>
+  );
+}
+
+export default Contact;
